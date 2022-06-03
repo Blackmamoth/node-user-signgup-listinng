03637ff58276024fa0de695e8ddbf8a2e1f0b7f0
blackmamoth <blackmamoth83@gmail.com>
2022-06-03 13:25:49 +0530

    integration of angular build and node backend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const cors = require('cors');
+const cors = require("cors");
+const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,10 +9,20 @@ const app = express();
 
 connectDB();
 
-app.use(cors({origin: "http://localhost:4200"}))
+app.use(cors({ origin: "/" }));
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/users", require("./routes/userRoutes"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/users", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
