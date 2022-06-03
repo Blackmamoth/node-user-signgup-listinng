00effbcec8b34033fa48234205d1e074cd032457
blackmamoth <blackmamoth83@gmail.com>
2022-06-03 12:17:48 +0530

    cors added

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require('cors');
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 connectDB();
 
+app.use(cors({origin: "http://localhost:4200"}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
